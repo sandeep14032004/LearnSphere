f52f09968b6a17cffb6aa77289c1946572fdb428
sandeep14032004 <sandeeppati69@gmail.com>
2025-01-17 15:24:20 +0530

    added..user authentication

---
+++ server/middlewares/isAuth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+export const isAuth = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    if (!token)
+      return res.status(403).json({ message: "You are not authorized" });
+    const decodedData = jwt.verify(token, process.env.Jwt_Sec);
+    req.user = await User.findById(decodedData._id);
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "You are not authorized" });
+  }
+};
