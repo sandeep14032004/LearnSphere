36b40392ab9f7201a228282108eb62010118f502
sandeep14032004 <sandeeppati69@gmail.com>
2025-03-16 20:52:13 +0530

    final

--- frontend/src/pages/courses/Courses.jsx
+++ frontend/src/pages/courses/Courses.jsx
@@ -2,17 +2,32 @@ import React from "react";
 import "./courses.css";
 import { CourseData } from "../../context/CourseContext";
 import CourseCard from "../../components/coursecard/CourseCard";
+import { useNavigate } from "react-router-dom";
+import { UserData } from "../../context/UserContext";
+
 const Courses = () => {
   const { courses } = CourseData();
-  console.log(courses);
+  const { user } = UserData();
+  const navigate = useNavigate();
+
   return (
     <div className="courses">
       <h2>Available Courses</h2>
+
+      {user?.role === "admin" && (
+        <button
+          className="common-btn"
+          onClick={() => navigate("/admin/course")}
+        >
+          Create Course
+        </button>
+      )}
+
       <div className="course-container">
         {courses && courses.length > 0 ? (
           courses.map((e) => <CourseCard key={e._id} course={e} />)
         ) : (
-          <p>No Courses.Yet</p>
+          <p>No Courses Yet</p>
         )}
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -3,8 +3,19 @@ import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 import Razorpay from "razorpay";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
+
+const uploadDir = path.join(path.resolve(), "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+  console.log("✅ 'uploads' directory created.");
+} else {
+  console.log("✅ 'uploads' directory already exists.");
+}
+
 export const instance = new Razorpay({
   key_id: process.env.Razorpay_key,
   key_secret: process.env.Razorpay_Secret,
@@ -15,12 +26,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.use("/uploads", express.static("uploads"));
+
+app.use("/uploads", express.static(uploadDir));
+
 import userRoutes from "./routes/user.js";
 import courseRoutes from "./routes/course.js";
 import adminRoutes from "./routes/admin.js";
@@ -30,6 +43,6 @@ app.use("/api", courseRoutes);
 app.use("/api", adminRoutes);
 
 app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
+  console.log(`🚀 Server is running on http://localhost:${port}`);
   connectDb();
 });
